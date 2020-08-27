c56b9bccf160ade6480c136561bab4e21edcb13e
tickstudiu <tickstudiu@gmail.com>
2020-08-27 16:42:13 +0700

    update btn

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-if="$auth.loggedIn">
+      {{ $auth.user.email }}
+      <b-button variant="danger">logout</b-button>
+    </div>
+    <div v-else>
+      not logged in
+      <b-button variant="primary">login</b-button>
+      <b-button variant="outline-primary">register</b-button>
+    </div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+</style>
